b66793505f44bc551a567e1764f06f52054eb250
SCRAPPYDOO <scrapek@vp.pl>
2009-08-30 23:55:34 +0200

    [3]Hyjal: Anetheron

---
+++ Hyjal/Hyjal_Anetheron.sql
@@ -0,0 +1,3 @@
+-- Anetheron
+UPDATE `creature_template` SET `mechanic_immune_mask` = '1073463287', `AIName` = '', `Scriptname` ='boss_anetheron' WHERE `entry` = '17808';
+UPDATE `creature_template` SET `mechanic_immune_mask` = '1073741823', `flags_extra` = '256', `AIName` = '', `Scriptname` ='mob_towering_infernal' WHERE `entry` = '17818';
