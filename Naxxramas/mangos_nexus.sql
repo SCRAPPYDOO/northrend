679b789dc8445453c21bc189a4c9f3becc811306
SCRAPPYDOO <scrapek@vp.pl>
2009-09-04 15:22:25 +0200

    [8]Naxxramas: Anubrekhan Add's Correct Lvl

---
+++ Naxxramas/mangos_nexus.sql
@@ -0,0 +1,7 @@
+-- Arachnid Quarter 
+
+-- AnubRekhan
+
+-- Crypt Guard lvl up to 79-81 id 16573
+UPDATE creature_template SET `minlevel` = '79', `maxlevel` = '81' WHERE `entry` IN (16573, 29256);
+
