f4e12a83d7dfed46c0dedff9be3419992add0cc6
SCRAPPYDOO <scrapek@vp.pl>
2009-12-03 19:34:00 +0100

    [18] Muru 99% Completed

--- Sunwell Plateu/r1436_a268_mangos_sunwell.sql
+++ Sunwell Plateu/r1436_a268_mangos_sunwell.sql
@@ -15,3 +15,17 @@ UPDATE creature_template SET `minlevel` = '70', `maxlevel` = '70',`ScriptName` =
 UPDATE creature_template SET `ScriptName` = 'mob_felmyst_trail' WHERE `entry` IN (25267); 
 
 UPDATE creature_template SET `ScriptName` = 'mob_deathcloud' WHERE `entry` IN (25703); 
+
+-- Muru Event
+-- Muru and Entropius
+UPDATE `creature_template` SET `lootid` = '25840', `mingold` = '2250000', `maxgold` = '2750000', `minhealth` = '5200000', `maxhealth` = '5200000', `mechanic_immune_mask` = '1073463287', `ScriptName` = 'boss_muru' WHERE `entry` IN (25741);
+
+-- Dark Fiend
+UPDATE `creature_template` SET `faction_A` = '14', `faction_H` = '14', `speed` = '1', `ScriptName` = 'dark_fiend' WHERE `entry` IN (25744); 
+
+-- Void Sentinel
+UPDATE `creature_template` SET `mechanic_immune_mask` = '1073463287', `AIName` = '', `ScriptName` = 'mob_voidsentinel' WHERE `entry` IN (25772); 
+UPDATE `creature_template` SET `minmana` = '10000', `maxmana` = '10000' WHERE `entry` IN (25798, 25772);
+
+-- Singularity
+UPDATE creature_template SET `faction_A` = '14', `faction_H` = '14', `speed` = '1', `rank` = '1', `AIName` = '', `ScriptName` = 'mob_singularity' WHERE `entry` IN (25855); 
